df7437a6641e9f5964f7178cedff0c14a71825b4
Milan Hauth <milahu@gmail.com>
2025-09-06 18:06:05 +0200

    remove pyside6-stubs

--- shell.nix
+++ shell.nix
@@ -7,7 +7,7 @@ pkgs.mkShell {
     (python3.withPackages (pp: with pp; [
       pyside6
       # pyside6-stubs # FIXME missing
-      nur.repos.milahu.python3.pkgs.pyside6-stubs
+      # nur.repos.milahu.python3.pkgs.pyside6-stubs
       tree-sitter
       # tree-sitter-languages # broken
       tree-sitter-language-pack
